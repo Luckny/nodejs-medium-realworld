8e5c550e0fec7a0a4b528684bba9a2bf92e17787
Luckny Schnider <sluckny@gmail.com>
2022-01-07 15:45:31 -0500

    fixed user Schema

--- models/users.js
+++ models/users.js
@@ -3,27 +3,24 @@ const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose')
 
 const userSchema = new Schema({
-    user: {
-        username: {
-            type: String,
-        },
-        token: {
-            type: String,
-        },
 
-        email: {
-            type: String,
-        },
-        bio: {
-            type: String,
-            default: 'No Bio.'
-        },
+    token: {
+        type: String,
+    },
 
-        image: {
-            type: String,
-            default: null
-        }
+    email: {
+        type: String,
+    },
+    bio: {
+        type: String,
+        default: 'No Bio.'
+    },
+
+    image: {
+        type: String,
+        default: null
     }
+
 })
 
 userSchema.plugin(passportLocalMongoose);

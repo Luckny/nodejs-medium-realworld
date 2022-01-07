7e0f6303cedc1edb44ffaf45ae1a7c8fbe216c05
Luckny Schnider <sluckny@gmail.com>
2022-01-07 14:55:30 -0500

    added some logic in app.js to test my users model

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const User = require('./models/users')
+const loaders = require('./loaders');
+const app = express();
+
+async function startServer() {
+
+
+    await loaders.init();
+
+    app.listen(3000, () => {
+        console.log('Serving on port 3000')
+    })
+
+}
+
+
+startServer();
+
+

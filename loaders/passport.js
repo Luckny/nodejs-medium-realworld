fbf559d89a5e323f286a83422aaee235461c0f92
Luckny <schnider08@hotmail.com>
2022-01-10 19:10:55 -0500

    this file contains the logic behind the passport-jwt strategy

---
+++ loaders/passport.js
@@ -0,0 +1,23 @@
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
+const { User } = require('../models')
+require('dotenv').config();
+
+const opts = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: process.env.JWT_SECRET
+}
+
+const strategy = new JwtStrategy(opts, (payload, done) => {
+    User.findOne({ id: payload.sub }, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false)
+        }
+    })
+})
+
+module.exports = strategy;

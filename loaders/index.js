007dc1767fb4b658f18b64c0700ff92017e72b17
Luckny <schnider08@hotmail.com>
2022-01-11 12:09:59 -0500

    removed loaders file

--- loaders/index.js
+++
@@ -1,6 +0,0 @@
-const mongooseLoader = require('./mongoose');
-
-module.exports.init = async () => {
-    const mongoConnection = await mongooseLoader();
-    console.log('MongoDb Initialized')
-}
--- loaders/mongoose.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const mongooseLoader = async () => {
-    const connection = await mongoose.connect('mongodb://localhost:27017/realworld-app', { useNewUrlParser: true, useUnifiedTopology: true });
-    return connection.connection.db;
-}
-
-module.exports = mongooseLoader;

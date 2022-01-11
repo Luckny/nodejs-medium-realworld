2015a8c8d2c6a9e1ae15e19e8644ecf2ce3501ef
Luckny <schnider08@hotmail.com>
2022-01-11 11:22:17 -0500

    added comments for clarity

---
+++ config/index.js
@@ -0,0 +1,9 @@
+/*****************************************************************
+ *      This file requires and exports all the library configs
+ *****************************************************************/
+const mongooseLoader = require('./mongoose');
+
+module.exports.init = async () => {
+    const mongoConnection = await mongooseLoader();
+    console.log('MongoDb Initialized')
+}

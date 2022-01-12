623eedbf9bf973f56567542a21724b3d57693413
Luckny <schnider08@hotmail.com>
2022-01-11 20:19:40 -0500

    removed config/index.js file

--- config/index.js
+++
@@ -1,9 +0,0 @@
-/*****************************************************************
- *      This file requires and exports all the library configs
- *****************************************************************/
-const mongooseLoader = require('./mongoose');
-
-module.exports.init = async () => {
-    const mongoConnection = await mongooseLoader();
-    console.log('MongoDb Initialized');
-}

e2146bdbfa742d3dc409551a56f2907271c28ae7
Luckny <schnider08@hotmail.com>
2022-01-11 20:22:31 -0500

    minor changes"

--- config/mongoose.js
+++ config/mongoose.js
@@ -9,17 +9,3 @@ module.exports.connectDb = async () => {
     await mongoose.connect(dbURI, options);
     console.log('MongoDB connected!')
 }
-
-
-
-
-// /**mongoose.connect(dbURI, options);
-//  * This function connects to mongodb
-//  * @returns The mongoDB connection
-//  */
-// const mongooseLoader = async () => {
-//     const connection = await mongoose.connect('mongodb://127.0.0.1:27017/realworld-app', { useNewUrlParser: true, useUnifiedTopology: true });
-//     return connection.connection.db;
-// }
-
-// module.exports = mongooseLoader;

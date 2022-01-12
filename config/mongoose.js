458c7c7713759f9163b6de43de9cb1938e7e19e7
Luckny <schnider08@hotmail.com>
2022-01-11 20:17:22 -0500

    started using env variable for db connection"

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose');
+const dbURI = 'mongodb://127.0.0.1:27017/realworld-app';
+if (process.env.NODE_ENV === 'production') {
+    dbURI = process.env.DB_URI;
+}
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
 
-/**
- * This function connects to mongodb
- * @returns The mongoDB connection
- */
-const mongooseLoader = async () => {
-    const connection = await mongoose.connect('mongodb://127.0.0.1:27017/realworld-app', { useNewUrlParser: true, useUnifiedTopology: true });
-    return connection.connection.db;
+module.exports.connectDb = async () => {
+    await mongoose.connect(dbURI, options);
+    console.log('MongoDB connected!')
 }
 
-module.exports = mongooseLoader;
+
+
+
+// /**mongoose.connect(dbURI, options);
+//  * This function connects to mongodb
+//  * @returns The mongoDB connection
+//  */
+// const mongooseLoader = async () => {
+//     const connection = await mongoose.connect('mongodb://127.0.0.1:27017/realworld-app', { useNewUrlParser: true, useUnifiedTopology: true });
+//     return connection.connection.db;
+// }
+
+// module.exports = mongooseLoader;

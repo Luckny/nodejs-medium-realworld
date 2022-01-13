44cf9e3a808dca20dc42562fafa167b478690af4
Luckny <schnider08@hotmail.com>
2022-01-13 09:46:12 -0500

    added updateUser route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const users = require('../controller/users'); //For all the user functions.
 const router = express.Router(); //The Router library.
+const {verifyToken} = require('../middleware')
 require('../config/passport');
 
 /******************************
@@ -10,7 +11,8 @@ require('../config/passport');
 router.post('/users', users.register);
 router.post('/users/login', users.login);
 router.route('/user')
-    .get(passport.authenticate('jwt', {session: false}), users.currentUser)
+    .get(passport.authenticate('jwt', { session: false }), verifyToken, users.currentUser)
+    .put(passport.authenticate('jwt', {session : false }), verifyToken, users.updateUser)
   
 
 //End Routes

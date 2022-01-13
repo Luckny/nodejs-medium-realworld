6cede599a3f08b17df17d2155b42f0412f0b36fc
Luckny <schnider08@hotmail.com>
2022-01-13 13:19:40 -0500

    added new useful functions

--- config/utils.js
+++ config/utils.js
@@ -49,3 +49,37 @@ module.exports.makeJsonError = (message) => {
    };
    return errors;
 };
+
+/**************************************************************
+ *    THIS FUNCTION VERIFIES AND OBJECT IS EMPTY OR NOT       *
+ **************************************************************/
+module.exports.isEmpty = (obj) => {
+   return Object.keys(obj).length === 0;
+};
+
+/**************************************************************
+ *    THIS FUNCTION VERIFIES IF THE TWO OBJECTS HAVE AT LEAT
+ *                ONE VALUE THAT REPEATS                      *
+ **************************************************************/
+module.exports.hasSameValue = (firstObj, secondObj) => {
+   let isSameValue = false;
+   let sameField = null;
+   //for the key value pairs in the firstObj
+   for (const keyInFirst in firstObj) {
+      //for the key value pairs in the secondObj
+      for (const keyInSecond in secondObj) {
+         if (
+            keyInFirst === keyInSecond &&
+            firstObj[keyInFirst] === secondObj[keyInSecond]
+         ) {
+            isSameValue = true;
+            sameField = keyInFirst;
+         }
+      }
+   }
+
+   return {
+      isSameValue,
+      sameField,
+   };
+};

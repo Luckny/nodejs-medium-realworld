d8a24bc07da5677040a805863a38eaf1a896d14d
Luckny <schnider08@hotmail.com>
2022-01-13 09:45:00 -0500

    made a middleware to verify the token and extract the info

---
+++ middleware.js
@@ -0,0 +1,19 @@
+const utils = require('./config/utils');
+const jwt = require('jsonwebtoken');
+
+module.exports.verifyToken = async (req, res, next) => {
+   //I need to get the user id from the  token
+   const token = req.headers.authorization.split(' ')[1];
+   //verifying the token will return the payload
+   const payload = jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+      //if a token error
+      if (err) {
+         next(err);
+      }
+      //if valid
+      return payload;
+   });
+
+   req.tokenAndPayload = { token, payload };
+   next();
+};

13fd3f216adca97b260e8ab0fecf00562b6b2fe5
Luckny <schnider08@hotmail.com>
2022-01-14 16:02:45 -0500

    added a line on top to help debug

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+process.on('warning', (e) => console.warn(e.stack));
 require('dotenv').config(); //to use .env variables
 const express = require('express');
-const mongoose = require('./config/mongoose');//Contains the mongoose configuration
-require('./config/passport')//For Authentification
+const mongoose = require('./config/mongoose'); //Contains the mongoose configuration
+require('./config/passport'); //For Authentification
 const { urlencoded } = require('express');
 const { userRoutes } = require('./routes');
 const app = express();
@@ -9,35 +10,27 @@ const app = express();
 /******************************
  *  APPLICATION MIDDLEWARES   *
  ******************************/
-app.use(urlencoded({ extended: true }))
-app.use(express.json())
+app.use(urlencoded({ extended: true }));
+app.use(express.json());
 //End MiddleWares.
 
-
-
 /**
- * This function uses the mongoose config in the loaders/index to 
+ * This function uses the mongoose config in the loaders/index to
  * Start the mongoose connection and then sets the listening port
  * for the express application.
  */
 async function startServer(port) {
+   await mongoose.connectDb();
 
-
-    await mongoose.connectDb();
-
-    app.listen(port, () => {
-        console.log('Serving on port 3000')
-    })
-
+   app.listen(port, () => {
+      console.log('Serving on port 3000');
+   });
 }
-
 
 /******************************
  *          ROUTES            *
  ******************************/
 app.use('/', userRoutes);
-
-
 
 //Starts the Server
 startServer(3000);

e243433c9316c05d5542096a537f800ffb82054a
Luckny <schnider08@hotmail.com>
2022-01-14 19:18:43 -0500

    implemented express-jwt strategy for authentification

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,31 @@
  **************************************************************************/
 const jwt = require('express-jwt');
 
-const options = {
-   secret: process.env.JWT_SECRET,
-   algorithms: ['HS256'],
+const getTokenFromHeaders = (req) => {
+   const {
+      headers: { authorization },
+   } = req;
+
+   if (authorization && authorization.split(' ')[0] === 'Token') {
+      return authorization.split(' ')[1];
+   }
+   return null;
 };
+
+const auth = {
+   required: jwt({
+      secret: process.env.JWT_SECRET,
+      algorithms: ['HS256'],
+      requestProperty: 'payload',
+      getToken: getTokenFromHeaders,
+   }),
+   optional: jwt({
+      secret: process.env.JWT_SECRET,
+      algorithms: ['HS256'],
+      requestProperty: 'payload',
+      getToken: getTokenFromHeaders,
+      credentialsRequired: false,
+   }),
+};
+
+module.exports = auth;

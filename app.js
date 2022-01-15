d5aecd0b625b7714667a407466f08754e59f77cc
Luckny <schnider08@hotmail.com>
2022-01-14 19:18:04 -0500

    added error handler for unexpected 401 codes

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ process.on('warning', (e) => console.warn(e.stack));
 require('dotenv').config(); //to use .env variables
 const express = require('express');
 const mongoose = require('./config/mongoose'); //Contains the mongoose configuration
-require('./config/passport'); //For Authentification
 const { urlencoded } = require('express');
 const { userRoutes } = require('./routes');
+const utils = require('./config/utils');
 const app = express();
 
 /******************************
@@ -31,6 +31,16 @@ async function startServer(port) {
  *          ROUTES            *
  ******************************/
 app.use('/', userRoutes);
+
+//Error handler
+app.use((err, req, res, next) => {
+   console.log('made it to handler');
+   if (err && err.status === 401) {
+      return res
+         .status(err.status)
+         .json(utils.makeJsonError('Unexpected Error'));
+   }
+});
 
 //Starts the Server
 startServer(3000);

3e7a28e379185f6a09a791b50e1473aee638671b
Luckny <schnider08@hotmail.com>
2022-01-15 18:07:38 -0500

    exporting profileRoutes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@
  *      This file requires and exports all the Routes
  *****************************************************************/
 const userRoutes = require('./users');
+const profileRoutes = require('./profiles');
 
 module.exports = {
    userRoutes,
+   profileRoutes,
 };

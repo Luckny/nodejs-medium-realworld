25d99790dcdaa2fd602bceb22539ff326e0f757e
Luckny <schnider08@hotmail.com>
2022-01-14 19:24:09 -0500

    now using the express-jwt for auth routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const passport = require('passport');
 const users = require('../controller/users'); //For all the user functions.
 const router = express.Router(); //The Router library.
-const {verifyToken} = require('../middleware')
-require('../config/passport');
+const auth = require('../config/auth');
 
 /******************************
  *          ROUTES            *
@@ -11,8 +9,8 @@ require('../config/passport');
 router.post('/users', users.register);
 router.post('/users/login', users.login);
 router.route('/user')
-    .get(passport.authenticate('jwt', { session: false }), verifyToken, users.currentUser)
-    .put(passport.authenticate('jwt', {session : false }), verifyToken, users.updateUser)
+    .get(auth.required, users.currentUser)
+    .put(auth.required, users.updateUser)
   
 
 //End Routes

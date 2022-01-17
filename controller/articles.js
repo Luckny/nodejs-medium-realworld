c34cdcaff41f42eb6278dbd9834a5d16727f6b98
Luckny <schnider08@hotmail.com>
2022-01-17 11:03:34 -0500

    implementing logic to create an article

--- controller/articles.js
+++ controller/articles.js
@@ -1,6 +1,9 @@
-const { Article } = require('../models'); //The User model.
+const { Article, User } = require('../models'); //The User model.
 const utils = require('../config/utils'); //Hashing library.
 const { StatusCodes } = require('http-status-codes');
+const randomCharacters = require('unique-slug');
+const makeSlug = require('slug');
+const print = console.log.bind(console, '>');
 
 /**
  * This function gets the recent articles from the users
@@ -14,14 +17,40 @@ module.exports.userFeed = async (req, res) => {
  * This function gets all the recent articles globally
  */
 module.exports.getAll = async (req, res) => {
+   //get the filter from hte query
+   const filter = req.query;
+   console.log(filter);
    res.send('All the articles');
 };
 
 /**
  * This function creates an article
+ * NB: i will use the Mongodb _id as slug
  */
 module.exports.createOne = async (req, res) => {
-   res.send('Creating one...');
+   const { title, description, body, tagList } = req.body.article;
+   const {
+      payload: { id },
+   } = req;
+
+   //getting the logged in user
+   const loggedInUser = await User.findById(id);
+   //creating the new article
+   const slug = makeSlug(`${title} ${randomCharacters()}`);
+   const article = new Article({
+      slug,
+      title,
+      description,
+      body,
+      tagList,
+      author: loggedInUser,
+   });
+
+   const createdArticle = await article.save();
+
+   return res
+      .status(StatusCodes.OK)
+      .json(createdArticle.toAPIJson(loggedInUser, loggedInUser));
 };
 
 /**

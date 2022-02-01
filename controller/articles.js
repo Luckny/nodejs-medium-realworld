a0df71efef3131177b21c22ace562f54ec726d00
Luckny Schnider <sluckny@gmail.com>
2022-01-31 23:24:09 -0500

    resolving conflict for pr

--- controller/articles.js
+++ controller/articles.js
@@ -1,26 +1,26 @@
-const { Article, User } = require('../models'); //The User model.
-const utils = require('../config/utils'); //Hashing library.
-const { StatusCodes } = require('http-status-codes');
-const randomCharacters = require('unique-slug');
-const makeSlug = require('slug');
-const print = console.log.bind(console, '>');
+const { Article, User } = require("../models"); //The User model.
+const utils = require("../config/utils"); //Hashing library.
+const { StatusCodes } = require("http-status-codes");
+const randomCharacters = require("unique-slug");
+const makeSlug = require("slug");
+const print = console.log.bind(console, ">");
 
 /**
  * This function gets the recent articles from the users
  * that the current user follows
  */
 module.exports.userFeed = async (req, res) => {
-   res.send('The current user feed');
+  res.send("The current user feed");
 };
 
 /**
  * This function gets all the recent articles globally
  */
 module.exports.getAll = async (req, res) => {
-   //get the filter from hte query
-   const filter = req.query;
-   console.log(filter);
-   res.send('All the articles');
+  //get the filter from hte query
+  const filter = req.query;
+  console.log(filter);
+  res.send("All the articles");
 };
 
 /**
@@ -28,69 +28,75 @@ module.exports.getAll = async (req, res) => {
  * NB: i will use the Mongodb _id as slug
  */
 module.exports.createOne = async (req, res) => {
-   const { title, description, body, tagList } = req.body.article;
-   const {
+  try {
+    const { title, description, body, tagList } = req.body.article;
+    const {
       payload: { id },
-   } = req;
+    } = req;
 
-   //getting the logged in user
-   const loggedInUser = await User.findById(id);
-   //creating the new article
-   const slug = makeSlug(`${title} ${randomCharacters()}`);
-   const article = new Article({
-      slug,
+    //getting the logged in user
+    const loggedInUser = await User.findById(id);
+    //creating the new article
+    const article = new Article({
+      slug: "newslug",
       title,
       description,
       body,
       tagList,
       author: loggedInUser,
-   });
+    });
 
-   const createdArticle = await article.save();
+    const createdArticle = await article.save();
 
-   return res
+    return res
       .status(StatusCodes.OK)
       .json(await createdArticle.toAPIJson(loggedInUser));
+  } catch (e) {
+    console.log(e);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(utils.makeJsonError("Unexpected Error!"));
+  }
 };
 
 /**
  * This function fetches a particular article
  */
 module.exports.getOne = async (req, res) => {
-   try {
-      const {
-         params: { slug },
-         payload: { id },
-      } = req;
-      const article = Article.findOne({ slug });
-      //if article doesnt exist
-      if (!article) {
-         return res
-            .status(StatusCodes.NOT_FOUND)
-            .json(utils.makeJsonError('User Not Found!'));
-      }
-      print(slug, id);
+  try {
+    const {
+      params: { slug },
+      payload: { id },
+    } = req;
+    const article = Article.findOne({ slug });
+    //if article doesnt exist
+    if (!article) {
       return res
-         .status(StatusCodes.OK)
-         .json(article.toAPIJson(loggedInUser, loggedInUser));
-   } catch (e) {
-      console.log(e);
-      return res
-         .status(StatusCodes.INTERNAL_SERVER_ERROR)
-         .json(utils.makeJsonError('Unexpected Error!'));
-   }
+        .status(StatusCodes.NOT_FOUND)
+        .json(utils.makeJsonError("User Not Found!"));
+    }
+    print(slug, id);
+    return res
+      .status(StatusCodes.OK)
+      .json(article.toAPIJson(loggedInUser, loggedInUser));
+  } catch (e) {
+    console.log(e);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(utils.makeJsonError("Unexpected Error!"));
+  }
 };
 
 /**
  * This function Updates one article
  */
 module.exports.updateOne = async (req, res) => {
-   res.send('Updating one');
+  res.send("Updating one");
 };
 
 /**
  * This function Deletes one article
  */
 module.exports.destroyOne = async (req, res) => {
-   res.send('Destroying one');
+  res.send("Destroying one");
 };
